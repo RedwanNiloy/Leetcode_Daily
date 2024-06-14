7f0ff209aaa84ebc3c4e64545bea72870e067d40
RedwanNiloy <redwanniloy161293@gmail.com>
2024-06-15 04:22:32 +0600

    June 14 updated

---
+++ Daily Challenge/945. Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,77 @@
+Approach 1
+Using sort ()
+
+Time complexity: O(nlogn)
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        
+       unordered_map<int,int>mp;
+       int c=0;
+       sort(nums.begin(),nums.end());
+        //int prev=nums[0];
+       for(int i=1;i<nums.size();i++)
+       {
+        if(nums[i]<=nums[i-1])
+        {
+            c+=nums[i-1]+1-nums[i];
+            nums[i]=nums[i-1]+1;
+        }
+        
+
+
+       }
+           
+    
+
+
+
+
+
+return c;
+
+    }
+};
+
+
+Approach 2
+Using counting sort
+Time complexity : O(n)
+
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int n = nums.size();
+        int maxElement = 0;
+        int moves = 0;
+
+        for (int val : nums) {
+            maxElement = max(maxElement, val);
+        }
+
+        vector<int> count(n + maxElement, 0);
+
+        for (int val : nums) {
+            count[val]++;
+        }
+
+        for (int i = 0; i < count.size(); i++) {
+            if (count[i] <= 1) continue;
+
+            int extra = count[i] - 1;
+            count[i + 1] += extra;
+            count[i] = 1;
+            moves += extra;
+        }
+
+        return moves;
+    }
+};
+
+
+
+
+
+
