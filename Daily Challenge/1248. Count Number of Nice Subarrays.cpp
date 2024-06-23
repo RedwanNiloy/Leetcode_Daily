a67fda4dd056a5e5dfa561b812c81be968ceb822
RedwanNiloy <redwanniloy161293@gmail.com>
2024-06-24 01:34:26 +0600

    june 23

---
+++ Daily Challenge/1248. Count Number of Nice Subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+
+        int n       = nums.size();
+        int count   = 0;
+        int currSum = 0;
+        mp[currSum] = 1;
+
+        for(int i = 0; i < n; i++) {
+            currSum += (nums[i]%2); //if odd - 1, even - 0
+
+            if(mp.count(currSum - k)) {
+                count += mp[currSum-k];
+            }
+
+            mp[currSum]++;
+        }
+
+        return count;
+
+    }
+};
+
